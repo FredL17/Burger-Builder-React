8b12427666d14f4265fda73344f504683e82668a
Feifan17 <lin.2570@osu.edu>
2020-06-19 17:42:55 -0400

    used shouldComponentUpdate life cycle hook to improve render performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,31 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import Button from "../../UI/Button/Button";
 
-function OrderSummary(props) {
-  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+class OrderSummary extends Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+          {this.props.ingredients[igKey]}{" "}
+        </li>
+      );
+    });
     return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-        {props.ingredients[igKey]}{" "}
-      </li>
+      <Fragment>
+        <h3>Order Summary</h3>
+        <ul>{ingredientSummary}</ul>
+        <h4>Total Price: ${this.props.price}</h4>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.cancelOrder}>
+          Cancel
+        </Button>
+        <Button btnType="Success" clicked={this.props.continueOrder}>
+          Continue
+        </Button>
+      </Fragment>
     );
-  });
-  return (
-    <Fragment>
-      <h3>Order Summary</h3>
-      <ul>{ingredientSummary}</ul>
-      <h4>Total Price: ${props.price}</h4>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.cancelOrder}>
-        Cancel
-      </Button>
-      <Button btnType="Success" clicked={props.continueOrder}>
-        Continue
-      </Button>
-    </Fragment>
-  );
+  }
 }
 
 export default OrderSummary;
